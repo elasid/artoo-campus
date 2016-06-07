58dd315f96b18381a9204790eae2982d9eca4b3f
3895525483 <elsygjonaj@gmail.com>
2016-06-07 16:24:54 +0000

    indoo

--- client/subjects/init/app.js
+++ client/subjects/init/app.js
@@ -1,2 +1,13 @@
 // define app module
+var app =  angular
+ .module("artooInit",[]);
+ 
+ var app = angular
+ .module('artooInit')
+ .run(() =>{
+   console.info('My module is up running!');    
+ });
 // define a run function
+app.run(function(){
+    console.info('I run from the module in the var! ');
+});
